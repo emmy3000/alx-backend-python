89890762e553e2f1940c6344bbba9a4f9279160a
Emeka Emodi <emodiemeka@gmail.com>
2023-10-08 18:41:55 -0700

    feat: Add concat function with type annotations and docstrings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This module defines a function to concatenate two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two input strings.
+
+    Args:
+        str1 (str): The first input string.
+        str2 (str): The second input string.
+
+    Returns:
+        str: The concatenated string of 'str1' and 'str2'.
+    """
+    return str1 + str2

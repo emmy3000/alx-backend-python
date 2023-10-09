491bad9f90b6e3627e265d9effdbf1ce047b09eb
Emeka Emodi <emodiemeka@gmail.com>
2023-10-08 18:52:58 -0700

    feat: Add floor function with type annotations and docstrings

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+This script defines a function to calculate the floor of a float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Calculate the floor of a float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        int: The floor value of the input float.
+    """
+    return math.floor(n)

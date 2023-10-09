fcaac0633cf1e2ceaca09af1b56fa9236f846c4d
Emeka Emodi <emodiemeka@gmail.com>
2023-10-08 21:35:05 -0700

    feat: Added type annotations to zoom_array function

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+This script defines a function to zoom in on an array.
+"""
+
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Zoom in on an array by repeating its elements.
+
+    Args:
+        lst (Tuple): The input tuple.
+        factor (int, optional): The zoom factor. Defaults to 2.
+
+    Returns:
+        Tuple: The zoomed-in tuple.
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return tuple(zoomed_in)

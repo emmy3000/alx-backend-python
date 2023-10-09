0fbc9a132439f31564a0c623234944cc702e0c4d
Emeka Emodi <emodiemeka@gmail.com>
2023-10-08 19:01:36 -0700

    feat: Add to_str function with type annotations and docstrings

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This script defines a function to convert a float
+to its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)

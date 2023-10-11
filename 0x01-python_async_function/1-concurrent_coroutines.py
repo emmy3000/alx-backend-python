39b7654a85ac309765363e559638812a7f08d3e6
Emeka Emodi <emodiemeka@gmail.com>
2023-10-10 19:48:47 -0700

    feat(async): Implement asynchronous coroutines in 'wait_n' for generating random delays and concurrent execution

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,58 +1,42 @@
 #!/usr/bin/env python3
-'''
+"""
 Asynchronous coroutines for generating random delays
 and executing multiple coroutines concurrently.
-'''
+"""
 import asyncio
+from typing import List
 import random
-from typing import List, Union
+
+# Import coroutines function from '0-basic_async_syntax'
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_random(max_delay: Union[int, float] = 10) -> float:
-    '''
-    Asynchronous coroutine that waits for a random delay
-    between 0 and max_delay seconds (inclusive).
-
-    Args:
-        max_delay (Union[int, float]): The maximum delay in seconds.
-        Defaults to 10.
-
-    Returns:
-        float: The random delay.
-    '''
-    if max_delay < 0:
-        raise ValueError("max_delay must be a non-negative number")
-
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
-
-
-async def wait_n(n: int, max_delay: Union[int, float] = 10) -> List[float]:
-    '''
-    Execute the wait_random coroutine n times concurrently
-    with the specified max_delay.
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+    Asynchronous coroutine that generates random delays and
+    executes multiple coroutines concurrently.
 
     Args:
-        n (int): The number of times to execute wait_random concurrently.
-        max_delay (Union[int, float]): The maximum delay in seconds.
-        Defaults to 10.
+        n (int): The number of times to execute the coroutine concurrently.
+        max_delay (int, optional): The maximum delay in seconds (inclusive).
+            Defaults to 10.
 
     Returns:
-        List[float]: A list of the delays in ascending order.
-    '''
+        List[float]: A list of random delays in ascending order.
+    Raises:
+        ValueError: If 'n' is a negative integer.
+    """
     if n < 0:
         raise ValueError("n must be a non-negative integer")
 
-    # Create a list to store the delays
-    delays = []
+    tasks = []
+    for _ in range(n):
+        task = asyncio.create_task(wait_random(max_delay))
+        tasks.append(task)
 
-    # Create a list of tasks for concurrent execution
-    tasks = [wait_random(max_delay) for _ in range(n)]
+    results = []
+    for completed_task in asyncio.as_completed(tasks):
+        delay = await completed_task
+        results.append(delay)
 
-    # Wait for all tasks to complete
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        delays.append(delay)
-
-    return delays
+    return results

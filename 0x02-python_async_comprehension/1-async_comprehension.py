7c1c5f55c7cabe2f5a42ff412900526ebc5207bc
Emeka Emodi <emodiemeka@gmail.com>
2023-10-10 21:29:00 -0700

    feat(async): Implement async comprehension for generating random numbers

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Async Comprehension
+
+This module defines an asynchronous comprehension function
+that generatesa list of random numbers between 0 and 10
+using the async generator.
+"""
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Asynchronous comprehension function that generates
+    a list of random numbers between 0 and 10
+    using the async generator.
+
+    Returns:
+        List[float]: A list of random numbers between 0 and 10.
+    """
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result

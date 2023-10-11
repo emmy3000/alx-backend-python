21393171ad35ac0796d6498272cd2d7ecaa0e1b5
Emeka Emodi <emodiemeka@gmail.com>
2023-10-10 19:06:08 -0700

    feat: Implement asynchronous coroutine for random delay

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,16 +4,15 @@ Asynchronous coroutine for generating random delays.
 '''
 import asyncio
 import random
-from typing import Union
 
 
-async def wait_random(max_delay: Union[int, float] = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     '''
     Asynchronous coroutine that waits for a random delay
     between 0 and max_delay seconds (inclusive).
 
     Args:
-        max_delay (Union[int, float]): The maximum delay in seconds.
+        max_delay (int): The maximum delay in seconds.
         Defaults to 10.
 
     Returns:
@@ -22,6 +21,6 @@ async def wait_random(max_delay: Union[int, float] = 10) -> float:
     if max_delay < 0:
         raise ValueError("max_delay must be a non-negative number")
 
-    delay = random.uniform(0, max_delay)
+    delay = random.random() * max_delay
     await asyncio.sleep(delay)
     return delay

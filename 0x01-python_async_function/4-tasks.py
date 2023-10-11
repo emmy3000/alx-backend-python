867592b0e4022bb364f1f5f9087a327f6582cfee
Emeka Emodi <emodiemeka@gmail.com>
2023-10-10 20:20:33 -0700

    feat(async): Implement asynchronous function 'wait_n' for concurrent execution of 'task_wait_random'

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,35 +1,35 @@
 # !/usr/bin/env python3
-'''
+"""
 Create asyncio.Tasks for the wait_random coroutine
 multiple times concurrently.
-'''
+"""
 import asyncio
-from typing import List, Union
+from typing import List
 
-# Import wait_random from 0-basic_async_syntax
-wait_random = __import__('0-basic_async_syntax').wait_random
+# Import 'task_wait_random' from '3-tasks'
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: Union[int, float]) -> List[float]:
-    '''
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
     Create asyncio.Tasks for the wait_random coroutine n times concurrently
     with the specified max_delay.
 
     Args:
         n (int): The number of times to execute wait_random concurrently.
-        max_delay (Union[int, float]): The maximum delay in seconds.
+        max_delay (int): The maximum delay in seconds.
 
     Returns:
         List[float]: A list of the delays in ascending order.
-    '''
+    """
     if n < 0:
         raise ValueError("n must be a non-negative integer")
 
-    delays = []
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
 
-    for task in asyncio.as_completed(tasks):
-        delay = await task
+    delays = []
+    for completed_task in asyncio.as_completed(tasks):
+        delay = await completed_task
         delays.append(delay)
 
     return delays
